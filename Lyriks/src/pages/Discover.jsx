50ae82004f7e7b0ebffc341d3feef9a9da2960f9
Manoranjan1k <manasj790@gmail.com>
2024-04-16 22:48:38 +0530

    Discover updated

--- Lyriks/src/pages/Discover.jsx
+++ Lyriks/src/pages/Discover.jsx
@@ -1,3 +1,17 @@
-const Discover = () => <div>Discover</div>;
+import { Error, Loader, SongCard } from '../components';
+import { genres } from '../assets/constants';
+
+const Discover = () => {
+  const genreTitle = 'Pop';
+
+  return (
+    <div className="flex flex-col"> n
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
+        <select> </select>
+      </div>
+    </div>
+  )
+};
 
 export default Discover;
